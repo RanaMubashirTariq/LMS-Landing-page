293d69d376505e843d403ac6e1c6d313834a2c41
Rana Mubashir Tariq <mubashirtariq.1366@gmail.com>
2025-09-09 12:59:35 +0500

    add new features

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ import HeroSection from "./screens/HeroSection/HeroSection";
 
 export default function Home() {
   return (
-    <div className="w-full overflow-hidden">
+    <div className="w-full overflow-hidden bg-white">
             <div id="#">
             <NavbarSection/>
             <HeroSection/>
